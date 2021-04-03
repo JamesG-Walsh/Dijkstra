f846f812ebcb8bd8f36f38d410df321e098f4411
James Walsh <jamesgcwalsh@gmail.com>
2021-04-03 14:39:38 -0400

    skeleton code

---
+++ heap.py
@@ -0,0 +1,24 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+class heap:
+    def __init__(self, keys, n):
+        1
+
+    def __in_heap(self, id):
+        2
+
+    def __min_key(self):
+        3
+
+    def __mind_id(self):
+        4
+
+    def __key(self, id):
+        5
+
+    def __delete_min(self):
+        6
+
+    def __decrease_key(self, id, new_key):
+        7
